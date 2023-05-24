ef05eb1605ea16ad4fa2cea196bfdc09a98aec89
DESKTOP-ENRHNU6\User <cutecat8110@gmail.com>
2023-05-24 16:20:47 +0800

    feat: 安裝 I18n

--- components/page/PageHeader.vue
+++ components/page/PageHeader.vue
@@ -1,11 +1,33 @@
 <template>
   <header class="sticky top-0 bg-white shadow">
-    <div class="container flex min-h-[3.5rem] items-center gap-4 overflow-hidden">
-      <NuxtLink to="/">Header</NuxtLink>
-      <NuxtLink to="/about">前往 About</NuxtLink>
-      <NuxtLink to="/contact">前往 Contact</NuxtLink>
-      <NuxtLink to="/test404">前往 404</NuxtLink>
-      <NuxtLink to="/pinia">前往 pinia</NuxtLink>
+    <div class="container flex min-h-[3.5rem] items-center justify-between overflow-hidden">
+      <nav class="flex gap-4">
+        <NuxtLink to="/">Header</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+        <NuxtLink to="/test404">404</NuxtLink>
+        <NuxtLink to="/pinia">pinia</NuxtLink>
+      </nav>
+      <div class="flex gap-4">
+        {{ $t('language') }}
+        <button
+          v-for="(localeItem, index) in locales"
+          :key="index"
+          type="button"
+          @click="changeLanguage(localeItem.code)"
+          class="underline"
+        >
+          {{ localeItem.code }}
+        </button>
+      </div>
     </div>
   </header>
 </template>
+
+<script setup>
+const { locale, locales, setLocaleCookie } = useI18n()
+const changeLanguage = (localeCode) => {
+  locale.value = localeCode
+  setLocaleCookie(localeCode)
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n'
+  ],
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
@@ -9,5 +14,16 @@ export default defineNuxtConfig({
 
   pinia: {
     autoImports: ['defineStore']
+  },
+
+  i18n: {
+    defaultLocale: 'zh',
+    langDir: 'locales',
+    locales: [
+      { code: 'en', file: 'en.json', iso: 'en-US', name: 'English' },
+      { code: 'zh', file: 'zh.json', iso: 'zh-TW', name: '繁體中文' }
+    ],
+    strategy: 'no_prefix',
+    vueI18n: './nuxt-i18n.js'
   }
 })
